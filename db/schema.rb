397f636eec63dab7286b117336aba2742bd4b8d5
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-08-07 19:09:21 -0500

    Change post body to content

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_033534) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "body"
+    t.string "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
