55fab356e47d4ae92b338f7f67526375c4b5aa39
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-08-20 23:01:25 -0500

    Add user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    email { 'default@default.com' }
-    username { 'defaultuser' }
+    email { Faker::Internet.email(name: "#{first_name} #{last_name}").downcase }
+    username { "#{first_name[0]}#{last_name}#{Faker::Number.number(digits: 3)}".downcase }
     password { 'default' }
-    first_name { 'firstname' }
-    last_name { 'lastname' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
