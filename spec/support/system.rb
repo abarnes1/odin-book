cbde63d00ff80fef5e67654dd0fe993e8c5a5a0c
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-08-20 23:04:06 -0500

    Allow dom_id method in system tests

--- spec/support/system.rb
+++ spec/support/system.rb
@@ -1,6 +1,11 @@
+require 'rails_helper'
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
-    # driven_by ENV['SHOW_BROWSER'] ? :selenium_chrome : :selenium_chrome_headless
+    # driven_by :rack_test
+    driven_by ENV['SHOW_BROWSER'] ? :selenium_chrome : :selenium_chrome_headless
   end
+
+  # allow use of dom_id method in tests
+  config.include ActionView::RecordIdentifier
 end
