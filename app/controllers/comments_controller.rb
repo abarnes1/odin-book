e5ceec32fbffaf5e749f36b6e3084c377b31eb77
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-08-25 19:56:03 -0500

    Add nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,32 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
-    @comment = Comment.new
+    @comment = Comment.new(comment_params)
+    # @comment = current_user.comments.build(comment_params)
   end
 
   def create
     @comment = current_user.comments.build(comment_params)
 
     if @comment.save
-      flash[:alert] = 'Post Creation Success'
+      flash[:alert] = 'Comment Creation Success'
       redirect_to posts_path
     else
-      flash[:alert] = 'Post Creation Error'
+      flash[:alert] = 'Comment Creation Error'
       # figure out how to render only the comment form
       # render :new, status: :unprocessable_entity
 
       redirect_to posts_path
     end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.remove_message
+    comment.save
+
+    redirect_to posts_path
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,10 @@ class Comment < ApplicationRecord
 
   has_many :comments, class_name: 'Comment', foreign_key: 'parent_comment_id'
   belongs_to :parent_comment, class_name: 'Comment', foreign_key: 'parent_comment_id', optional: true
+
+  scope :top_level, -> { where(parent_comment_id: nil) }
+
+  def remove_message
+    self.message = '(deleted comment)'
+  end
 end
