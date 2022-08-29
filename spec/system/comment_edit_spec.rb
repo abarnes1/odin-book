c4b974ea6ae93fea5781f9788bc9e6c721a19701
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-08-28 20:49:45 -0500

    Add comment edit and list post spec

--- spec/system/comment_edit_spec.rb
+++ spec/system/comment_edit_spec.rb
@@ -8,29 +8,49 @@ RSpec.describe 'editing a comment' do
   before do
     user.posts << post
     post.comments << comment
-    sign_in user
-
-    visit posts_path
-    click_on 'Edit'
   end
 
-  context 'when fields are not valid' do
-    it 'does not update the comment' do
-      fill_in 'Comment:', with: ''
-      click_on 'Update Comment'
-      expect(page).to have_content('Comment Update Error')
+  context 'when comment is from signed in user' do
+    before do
+      sign_in user
+      visit posts_path
+      click_on 'Edit'
+    end
+
+    context 'when fields are not valid' do
+      it 'does not update the comment' do
+        fill_in 'Comment:', with: ''
+        click_on 'Update Comment'
+        expect(page).to have_content('Comment Update Error')
+      end
+    end
+
+    context 'when fields are valid' do
+      let(:new_comment_message) { 'updated comment' }
+
+      it 'updates the comment' do
+        fill_in 'Comment:', with: new_comment_message
+        click_on 'Update Comment'
+
+        within '.comment-container' do
+          expect(page).to have_content(new_comment_message)
+        end
+      end
     end
   end
 
-  context 'when fields are valid' do
-    let(:new_comment_message) { 'updated comment' }
+  context 'when comment is from another user' do
+    let(:other_user) { create(:user) }
 
-    it 'updates the comment' do
-      fill_in 'Comment:', with: new_comment_message
-      click_on 'Update Comment'
+    before do
+      sign_in other_user
+      visit posts_path
+    end
 
+    it 'cannot be edited' do
+      pending 'will fail until post#index shows friend posts'
       within '.comment-container' do
-        expect(page).to have_content(new_comment_message)
+        expect(page).not_to have_selector(:link_or_button, 'Reply')
       end
     end
   end
---
+++ spec/system/like_post_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'liking a post' do
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post, user: user) }
+
+  before do
+    user.posts << post
+    sign_in user
+    visit posts_path
+  end
+
+  context 'when post is not liked' do
+    it 'can be liked' do
+      within '.post-container' do
+        expect(page).to have_button(value: 'Like')
+      end
+    end
+
+    it 'liking increments likes count' do
+      within '.post-container' do
+        expect(page).to have_content('Likes: 0')
+      end
+    end
+  end
+
+  context 'when post is liked' do
+    before do
+      click_on 'Like'
+    end
+
+    it 'can be unliked' do
+      within '.post-container' do
+        expect(page).to have_button(value: 'Unlike')
+      end
+    end
+
+    it 'liking decrements likes count' do
+      within '.post-container' do
+        expect(page).to have_content('Likes: 1')
+      end
+    end
+  end
+end
