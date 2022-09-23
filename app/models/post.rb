73b51cf8178cd44d5684d8d6e6a35d47b7fcccf9
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-09-22 19:20:05 -0500

    Update feed query

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy, inverse_of: :post
   has_many :comments, dependent: :destroy, inverse_of: :post
 
+  scope :newest, -> { order(created_at: :desc) }
+
   validates :content, presence: true
 
   def user_like(user)
@@ -12,4 +14,7 @@ class Post < ApplicationRecord
   def liked_by?(user)
     !!user_like(user)
   end
+
+  # temporary, may remove after benchmarking
+  has_many :feed_comments, -> { Comment.top_level.newest.limit(5) }, class_name: 'Comment', inverse_of: :post
 end
