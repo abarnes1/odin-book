8f7d911c59020603868de991cde353be87d1eaed
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-11-19 16:43:58 -0600

    Add PostPresenter tests

---
+++ spec/presenters/post_presenter_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe PostPresenter do
+  let(:user) { build_stubbed(:user) }
+  let(:post) { build_stubbed(:post, user: user) }
+
+  describe '#post_id' do
+    it 'returns the post id' do
+      presenter = described_class.new(post)
+      expect(presenter.post_id).to eq(post.id)
+    end
+  end
+
+  describe '#comment_id' do
+    it 'returns nil' do
+      presenter = described_class.new(post)
+      expect(presenter.comment_id).to be_nil
+    end
+  end
+
+  describe '#load_comments_link_text' do
+    it 'returns the correct string' do
+      not_displayed_count = 99
+      presenter = described_class.new(post)
+      allow(presenter).to receive(:not_displayed_comments_count).and_return(not_displayed_count)
+      
+      expected = "View Previous (#{not_displayed_count} Remain)"
+      expect(presenter.load_comments_link_text).to eq(expected)
+    end
+  end
+
+  describe '#display_comments_turbo_method' do
+    it 'returns prepend' do
+      presenter = described_class.new(post)
+      expect(presenter.display_comments_turbo_method).to eq(:prepend)
+    end
+  end
+end
