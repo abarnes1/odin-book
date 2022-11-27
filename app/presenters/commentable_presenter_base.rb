a7cbdd8baa4c00b25e4293aea03db9bddab645f5
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-11-26 20:02:29 -0600

    Add CommentablePresenterBase methods and tests

--- app/presenters/commentable_presenter_base.rb
+++ app/presenters/commentable_presenter_base.rb
@@ -13,11 +13,27 @@ class CommentablePresenterBase < SimpleDelegator
     commentable
   end
 
+  def display_depth
+    nil
+  end
+
   def comments_container_id
     "#{__getobj__.class.name.downcase}_#{id}_comments"
   end
 
+  def new_comments_container_id
+    "#{__getobj__.class.name.downcase}_#{id}_new_comments"
+  end
+
+  def comments_counter_container_id
+    "#{__getobj__.class.name.downcase}_#{id}_comments_counter"
+  end
+
   def load_comments_link_id
     "#{__getobj__.class.name.downcase}_#{id}_load_comments"
+  end
+
+  def comment_form_id
+    "#{__getobj__.class.name.downcase}_#{id}_comment_form"
   end
 end
--- spec/presenters/commentable_presenter_base_spec.rb
+++ spec/presenters/commentable_presenter_base_spec.rb
@@ -10,10 +10,39 @@ RSpec.describe CommentablePresenterBase do
     end
   end
 
+  context 'when given a displayed count' do
+    it 'returns the correct count' do
+      display_count = 99
+      presenter = described_class.new(thing, displayed_count: display_count)
+      expect(presenter.displayed_comments_count).to eq(display_count)
+    end
+  end
+
+  describe '#display_depth' do
+    it 'returns nil' do
+      presenter = described_class.new(thing)
+      expect(presenter.display_depth).to be_nil
+    end
+  end
+
   describe '#comments_container_id' do
     it 'returns the correct id' do
       presenter = described_class.new(thing)
       expect(presenter.comments_container_id).to eq("object_99_comments")
+    end
+  end
+
+  describe '#new_comments_container_id' do
+    it 'returns the correct id' do
+      presenter = described_class.new(thing)
+      expect(presenter.new_comments_container_id).to eq("object_99_new_comments")
+    end
+  end
+
+  describe '#comments_counter_container_id' do
+    it 'returns the correct id' do
+      presenter = described_class.new(thing)
+      expect(presenter.comments_counter_container_id).to eq("object_99_comments_counter")
     end
   end
 
@@ -24,11 +53,10 @@ RSpec.describe CommentablePresenterBase do
     end
   end
 
-  context 'when given a displayed count' do
-    it 'returns the correct count' do
-      display_count = 99
-      presenter = described_class.new(thing, displayed_count: display_count)
-      expect(presenter.displayed_comments_count).to eq(display_count)
+  describe '#comment_form_id' do
+    it 'returns the correct id' do
+      presenter = described_class.new(thing)
+      expect(presenter.comment_form_id).to eq("object_99_comment_form")
     end
   end
 end
