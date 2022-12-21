673e0f1b2f5af171c545de6698f0fb8945ee8ea3
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-12-21 17:59:52 -0600

    Add likes turbo streams

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,49 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  
-  def create
-    flash[:alert] = if likeable.likes.create(user: current_user)
-                      'Liked!'
-                    else
-                      'Like Failed'
-                    end
 
-    redirect_to feed_path
+  def create
+    if likeable.likes.create(user: current_user)
+      respond_to do |format|
+        format.turbo_stream do
+          likeable_presenter =
+            if likeable.is_a? Post
+              PostPresenter.new(likeable)
+            else
+              CommentPresenter.new(likeable)
+            end
+
+          render :create, locals: { likeable: likeable_presenter }
+        end
+        format.html { redirect_to feed_path, alert: 'Liked!' }
+      end
+    else
+      flash[:alert] = 'Like Failed'
+    end
   end
 
   def destroy
-    flash[:alert] = if like.destroy
-                      'Unliked!'
-                    else
-                      'Unlike Failed'
-                    end
+    if like.destroy
+      respond_to do |format|
+        format.turbo_stream do
+          likeable_presenter =
+            if likeable.is_a? Post
+              PostPresenter.new(likeable)
+            else
+              CommentPresenter.new(likeable)
+            end
 
-    redirect_to feed_path
+          render :destroy, locals: { likeable: likeable_presenter }
+        end
+        format.html { redirect_to feed_path, alert: 'Unliked!' }
+      end
+    else
+      flash[:alert] = 'Unlike Failed'
+    end
+  end
+
+  private
+
+  def user_owns_like?
+    like.user == current_user
   end
 end
