3503aa2ca487a3e513d600cc16203d1f0fc107fa
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-12-21 18:18:39 -0600

    Update misleading filename

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,7 @@ class LikesController < ApplicationController
   def create
     if likeable.likes.create(user: current_user)
       respond_to do |format|
-        format.turbo_stream { render :update_counters, locals: { likeable: likeable_presenter } }
+        format.turbo_stream { render :update, locals: { likeable: likeable_presenter } }
         format.html { redirect_to feed_path, alert: 'Liked!' }
       end
     else
@@ -15,7 +15,7 @@ class LikesController < ApplicationController
   def destroy
     if like.destroy
       respond_to do |format|
-        format.turbo_stream { render :update_counters, locals: { likeable: likeable_presenter } }
+        format.turbo_stream { render :update, locals: { likeable: likeable_presenter } }
         format.html { redirect_to feed_path, alert: 'Unliked!' }
       end
     else
