c6daf983a07a31d083933b530e8f19cf9667b283
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-01-26 17:42:48 -0600

    Add user comments page

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,6 +1,8 @@
 class Users::CommentsController < CommentsController
   def index
     @user = User.find(params[:user_id])
-    @comments = @user.comments.map { |comment| CommentPresenter.new(comment, { depth: 0 }) }
+    @comments = @user.comments
+                     .includes(post: :user, parent_comment: :user)
+                     .map { |comment| CommentPresenter.new(comment, { depth: 0 }) }
   end
 end
