36318264576aca433ba40417f5e31214ad401579
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-11 11:30:59 -0600

    Add Pageable tests

--- app/models/concerns/pageable.rb
+++ app/models/concerns/pageable.rb
@@ -1,3 +1,26 @@
+# frozen_string_literal: true
+
+# Adds #page and #pages scopes to model classes.
+# Will prevent asking for negative page numbers or negative per page values
+# as they would result in invalid SQL statements.  When invalid values
+# are requested they will be replaced with safe values that will result
+# in valid SQL.
+#
+# User.page(-1) => User.page(1)
+# User.page(2, -1) => User.page(2, 10), where 10 is the default per_page value
+#
+# Per page values may be overridden in model classes, however,
+# invalid values from overridden methods will result in an error rather
+# than defaulting to safe values, as invalid values should not be defined
+# at a class level.
+#
+# class User < ApplicationRecord
+#   def self.per_page
+#     -1
+#   end
+# end
+#
+# User.page(1) defaults to User.page(1, -1) and will raise an exception
 module Pageable
   extend ActiveSupport::Concern
 
@@ -17,14 +40,14 @@ module Pageable
       page_number
     end
 
-    def self.safe_per_page(per)
-      if per_page.negative? || per_page.zero?
-        raise RangeError, "#{ancestors.first} class override of #page_number must return a positive number."
+    def self.safe_per_page(per = nil)
+      return per unless per.nil? || per <= 0
+
+      if per_page.nil? || per_page <= 0
+        raise RangeError, "#{ancestors.first} class override of #per_page must return a positive number."
       end
 
-      return per_page if per.nil? || per <= 0
-
-      per
+      per_page
     end
 
     def self.safe_offset(page_number, per)
--- spec/models/concerns/pageable_spec.rb
+++ spec/models/concerns/pageable_spec.rb
@@ -21,20 +21,84 @@ RSpec.describe Pageable do
   end
 
   describe '#safe_page_number' do
-    context 'when page is negative' do
-      it 'returns the default per page value' do
+    context 'when page param is negative' do
+      it 'returns the first page' do
+        expect(dummy_class.safe_page_number(-1)).to eq(1)
       end
     end
 
-    context 'when page is zero' do
-      it 'returns the default per page value' do
-
+    context 'when page param is zero' do
+      it 'returns the first page' do
+        expect(dummy_class.safe_page_number(0)).to eq(1)
       end
     end
 
-    context 'when page is positive' do
-      it 'returns the provided page number' do
-        
+    context 'when page param is positive' do
+      it 'returns the page param' do
+        page_number = 123
+        expect(dummy_class.safe_page_number(page_number)).to eq(page_number)
+      end
+    end
+  end
+
+  describe '#safe_per_page' do
+    context 'when per param is nil' do
+      it 'returns the default per page' do
+        expect(dummy_class.safe_per_page).to eq(Pageable::PER_PAGE_DEFAULT)
+      end
+    end
+
+    context 'when per param is negative' do
+      it 'returns the default per page' do
+        expect(dummy_class.safe_per_page(-1)).to eq(Pageable::PER_PAGE_DEFAULT)
+      end
+    end
+
+    context 'when per param is zero' do
+      it 'returns the default per page' do
+        expect(dummy_class.safe_per_page(0)).to eq(Pageable::PER_PAGE_DEFAULT)
+      end
+    end
+
+    context 'when per param is positive' do
+      it 'returns the per param' do
+        per_page = 123
+        expect(dummy_class.safe_per_page(per_page)).to eq(per_page)
+      end
+    end
+
+    context 'when including class overrides #per_page' do
+      it 'raises an error with negative value' do
+        dummy_class.define_singleton_method(:per_page) { -1 }
+        expect{ dummy_class.safe_per_page }.to raise_error(StandardError)
+      end
+
+      it 'raises an error with zero value' do
+        dummy_class.define_singleton_method(:per_page) { 0 }
+        expect{ dummy_class.safe_per_page }.to raise_error(StandardError)
+      end
+
+      it 'returns per_page with positive value' do
+        dummy_class.define_singleton_method(:per_page) { 1 }
+        expect(dummy_class.safe_per_page).to eq(dummy_class.per_page)
+      end
+    end
+  end
+
+  describe '#safe_offset' do
+    context 'when params are positive' do
+      it 'calculates the offset' do
+        page = 3
+        per_page = 4
+        expected_offset = 8
+
+        expect(dummy_class.safe_offset(3, 4)).to eq(8)
+      end
+    end
+
+    context 'when params are unsafe values' do
+      it 'calculates the offset with default values' do
+        expect(dummy_class.safe_offset(-1, -1)).to eq(0)
       end
     end
   end
