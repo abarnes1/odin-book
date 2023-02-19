e576c55b693ff6109b309a6024e5145a81d0d73c
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-19 16:44:51 -0600

    Add pagination to user posts

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,6 +1,19 @@
 class Users::PostsController < PostsController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.map { |post| PostPresenter.new(post) }
+    posts_relation = @user.posts.newest
+
+    pagination = Pagination::RelationPagination.new(posts_relation)
+
+    @posts = pagination.page(page).map { |post| PostPresenter.new(post) }
+    @page_ranges = pagination.page_ranges
+  end
+
+  private
+
+  def page
+    return 1 unless params[:page].present?
+
+    params[:page].to_i
   end
 end
