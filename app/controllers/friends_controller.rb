ddbead913eda1cb6d494df1f8a71bc899f24de65
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-19 19:03:53 -0600

    Add pagination to friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,6 +2,18 @@ class FriendsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @friends = current_user.friends
+    friends_relation = current_user.friends.order(:username)
+    pagination = Pagination::RelationPagination.new(friends_relation)
+
+    @friends = pagination.page(page)
+    @page_ranges = pagination.page_ranges
+  end
+
+  private
+
+  def page
+    return 1 unless params[:page].present?
+
+    params[:page].to_i
   end
 end
