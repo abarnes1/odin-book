667fc74ebda4e138bb20a0ba57ee49be1b988762
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-19 19:04:50 -0600

    Add pagination to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,24 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.where.not(id: current_user.id).order(:username)
+    user_relation = User.where.not(id: current_user.id).order(:username)
+    pagination = Pagination::RelationPagination.new(user_relation)
+
+    @users = pagination.page(page)
+    @page_ranges = pagination.page_ranges
   end
 
   def show
     @user = User.find(params[:id])
     @post = PostPresenter.new(@user.posts.last) unless @user.posts.last.nil?
     @comment = CommentPresenter.new(@user.comments.last, { depth: 0 }) unless @user.comments.last.nil?
+  end
+
+  private
+
+  def page
+    return 1 unless params[:page].present?
+
+    params[:page].to_i
   end
 end
