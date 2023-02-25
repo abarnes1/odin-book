915fe8f03f0c9787fc8b01b0cab9ebfb43523723
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-25 01:23:18 -0600

    Update Pageable tests - Eliminate the chances that other model tests   can create model records that alter the return   values for #page and #pages.

--- spec/models/concerns/shared/pageable.rb
+++ spec/models/concerns/shared/pageable.rb
@@ -1,25 +1,26 @@
 RSpec.shared_examples Pageable do
-  # If these tests fail, check #count.  Some top level
-  # model tests may be creating records!
-
   describe '#pages' do
+    let(:ten_model_relation) { create_model_relation(10) }
+
     before do
-      create_model(10)
+      allow(described_class).to receive(:all).and_return(ten_model_relation)
     end
 
     it 'calculates the number of pages with default per value' do
       stub_const("Pageable::PER_PAGE_DEFAULT", 4)
-      expect(described_class.pages).to eq(3)
+      expect(described_class.all.pages).to eq(3)
     end
 
     it 'calculates the number of pages when per is specified' do
-      expect(described_class.pages(per: 3)).to eq(4)
+      expect(described_class.all.pages(per: 3)).to eq(4)
     end
   end
 
   describe '#page' do
+    let(:ten_model_relation) { create_model_relation(10) }
+
     before do
-      create_model(10)
+      allow(described_class).to receive(:all).and_return(ten_model_relation)
     end
 
     it 'returns the correct page' do
@@ -36,11 +37,14 @@ RSpec.shared_examples Pageable do
 
   private
 
-  def create_model(count)
+  def create_model_relation(count)
     factory_name = described_class.to_s.downcase
+    query_results = []
     
-    10.times do 
-      create(factory_name.to_sym)
+    count.times do 
+      query_results << create(factory_name.to_sym)
     end
+
+    described_class.where(id: query_results)
   end
 end
