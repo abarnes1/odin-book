961cb5df48f39b92fb24da35b81937b47e24cc30
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-25 00:57:25 -0600

    Move test setup within describe blocks

--- spec/models/concerns/shared/pageable.rb
+++ spec/models/concerns/shared/pageable.rb
@@ -1,12 +1,12 @@
 RSpec.shared_examples Pageable do
-  before do
-    factory_name = described_class.to_s.downcase
-    10.times do 
-      create(factory_name.to_sym)
-    end
-  end
+  # If these tests fail, check #count.  Some top level
+  # model tests may be creating records!
 
   describe '#pages' do
+    before do
+      create_model(10)
+    end
+
     it 'calculates the number of pages with default per value' do
       stub_const("Pageable::PER_PAGE_DEFAULT", 4)
       expect(described_class.pages).to eq(3)
@@ -18,15 +18,29 @@ RSpec.shared_examples Pageable do
   end
 
   describe '#page' do
+    before do
+      create_model(10)
+    end
+
     it 'returns the correct page' do
       stub_const("Pageable::PER_PAGE_DEFAULT", 3)
-      expect(described_class.page(page_number: 4)).to eq([described_class.last])
+      expect(described_class.page(4)).to eq([described_class.last])
     end
 
     context 'when page out of range' do
       it 'returns an empty array' do
-        expect(described_class.page(per: 1000)).to eq([])
+        expect(described_class.page(1000)).to eq([])
       end
+    end
+  end
+
+  private
+
+  def create_model(count)
+    factory_name = described_class.to_s.downcase
+    
+    10.times do 
+      create(factory_name.to_sym)
     end
   end
 end
