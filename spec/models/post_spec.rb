a9a169cab234f45cfa29f1537505a291f1a62d8b
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-02-27 16:17:24 -0600

    Add initial Post model tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,65 @@ require 'rails_helper'
 require_relative 'concerns/shared/pageable'
 
 RSpec.describe Post, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-
   it_behaves_like Pageable
+
+  describe '#comments_count' do
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post, user: user) }
+    let!(:comment) { create(:comment, post: post, user: user) }
+
+    context 'when top level comments are added' do
+      it 'increments the counter' do
+        expect(post.comments_count).to eq(1)
+      end
+    end
+
+    context 'when top level comments are removed' do
+      it 'decrements the counter' do
+        comment.destroy
+        expect(post.comments_count).to eq(0)
+      end
+    end
+
+    context 'when comment replies are added' do
+      it 'does not change' do
+        create(:comment, post: comment.post, parent_comment: comment)
+        expect(post.comments_count).to eq(1)
+      end
+    end
+  end
+
+  describe '#total_comments_count' do
+    let!(:user) { create(:user) }
+    let!(:post) { create(:post, user: user) }
+    let!(:comment) { create(:comment, post: post, user: user) }
+
+    context 'when top level comments are added' do
+      it 'increments the counter' do
+        expect(post.total_comments_count).to eq(1)
+      end
+    end
+
+    context 'when top level comments are removed' do
+      it 'decrements the counter' do
+        comment.destroy
+        expect(post.total_comments_count).to eq(0)
+      end
+    end
+
+    context 'when comment replies are added' do
+      it 'increments the counter' do
+        comment_reply = create(:comment, post: comment.post, parent_comment: comment)
+        expect(post.total_comments_count).to eq(2)
+      end
+    end
+
+    context 'when comment replies are removed' do
+      it 'decrements the counter' do
+        comment_reply = create(:comment, post: comment.post, parent_comment: comment)
+        comment_reply.destroy
+        expect(post.total_comments_count).to eq(1)
+      end
+    end
+  end
 end
