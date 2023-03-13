f50039cab308b843a695ec0b0965e764c6cc8127
Aaron Barnes <aaron.barnes1@hotmail.com>
2023-03-12 21:36:10 -0500

    Preload testing

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,17 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications.limit(10)
+
+    # works but can't reach beyond notifiable
+    # preloader = ActiveRecord::Associations::Preloader.new(
+    #   records: [@notifications].flatten,
+    #   associations: :notifiable
+    # ).call
+    
+    preloader = ActiveRecord::Associations::Preloader.new(
+      records: [@notifications].flatten,
+      # associations: [notifiable: [:user]]
+      associations: [notifiable: [:user, owner: [:user]]]
+    ).call
   end
 end
